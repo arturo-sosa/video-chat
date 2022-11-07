2a76b5632026b48eeac8ed4782e01961e75329e3
Arturo Sosa <61207851+arturo-sosa@users.noreply.github.com>
2022-11-07 17:28:03 -0600

    Room creation

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -1,38 +1,36 @@
 import 'solid-js';
-import { createEffect, createResource } from 'solid-js';
+import { createEffect, createResource, createSignal } from 'solid-js';
 import { useStream } from './Stream';
+import io from 'socket.io-client';
+import useRoom from '../state/roomState';
 
 const Room = () => {
   const userStream = useStream();
-  const [room] = createResource(async () => {
-    const params = new URLSearchParams(window.location.search);
-    const roomId = params.get('room');
-    const roomPrefix = roomId ? `room/${roomId}` : "room";
-    const request = await fetch(`http://localhost:3000/${roomPrefix}`);
-    const response = await request.json();
+  const roomState = useRoom();
+  const [isConnected, setConnected] = createSignal(false);
+  const [socket] = createSignal(io("http://localhost:3000"));
 
-    return response;
+  socket().on('connect', () => {
+    setConnected(true);
   });
 
-  const updateRoomQuery = () => {
-    if (!room.loading && !room.error) {
-      const baseUrl = `${window.location.protocol}//${window.location.host}`;
-      const params = new URLSearchParams(window.location.search);
-      params.set('room', room().id);
+  socket().on('disconnect', () => {
+    console.log('disconnected');
+    setConnected(false);
+  });
 
-      const newState = `${baseUrl}?${params}`;
-      window.history.pushState({ path: newState }, '', newState);
-    }
-  };
+  createEffect(() => {
+    if (isConnected() === false) return;
 
-  createEffect(updateRoomQuery);
+    socket().emit('hello', 'moooo');
+  })
 
   return <div class="h-8 flex">
     <div>
     {
-      room.loading
+        roomState().loading
         ? <div>Waiting for room</div>
-        : <div>{`Room ${room().id}`}</div>
+          : <div>{`Room ${roomState().id}`}</div>
     }
     </div>
 
