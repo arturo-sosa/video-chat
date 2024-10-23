a4adf579f225449a971d6aea737dd178a2a9b665
Arturo Sosa <git@arturososa.dev>
2024-10-23 11:28:19 -0600

    fix: video gap

--- frontend/src/components/VideoGrid.tsx
+++ frontend/src/components/VideoGrid.tsx
@@ -7,7 +7,7 @@ const VideoGrid = () => {
   const { streams } = useStream();
 
   return (
-    <div class="flex justify-center">
+    <div class="flex justify-center gap-4">
       <For each={streams}>
         {
           (stream) => <Video peer={stream.peerId} muted={stream.muted} stream={stream.mediaStream} />
